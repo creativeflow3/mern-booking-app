42f69aeea786c8b956a26c8bc748512f0058d0ab
Jonathan McNeil <creativeflow3@gmail.com>
2025-03-26 15:31:16 -0700

    add view deatials button

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -61,6 +61,14 @@ const MyHotels = () => {
                 {hotel.starRating} Star Rating
               </div>
             </div>
+            <span className="flex justify-end">
+              <Link
+                to={`/edit-hotel/${hotel._id}`}
+                className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
+              >
+                View Details
+              </Link>
+            </span>
           </div>
         ))}
       </div>
