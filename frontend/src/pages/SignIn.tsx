706c833480f152eb4c36262313f1b474c3944736
Jonathan McNeil <creativeflow3@gmail.com>
2025-03-28 19:56:28 -0700

    Add guest info form and detail form

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import { useMutation, useQueryClient } from 'react-query';
 import * as apiClient from '../api-client';
 import { useAppContext } from '../context/AppContext';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export type SignInFormData = {
   email: string;
@@ -12,6 +12,7 @@ export type SignInFormData = {
 const SignIn = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+  const location = useLocation();
   const {
     register,
     formState: { errors },
@@ -27,7 +28,7 @@ const SignIn = () => {
         type: 'SUCCESS',
       });
       await queryClient.invalidateQueries('validateToken');
-      navigate('/');
+      navigate(location.state?.from?.pathname || '/');
     },
     onError: (error: Error) => {
       showToast({ message: error.message, type: 'ERROR' });
